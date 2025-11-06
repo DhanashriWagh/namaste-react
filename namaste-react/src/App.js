c9e6bfd6097efccfb0f51c67ce11b1f1ca0fec71
Dhanashri Wagh <waghdhanashri1994@gmail.com>
2025-11-06 16:57:19 +0530

    Initial commit

---
+++ namaste-react/src/App.js
@@ -0,0 +1,764 @@
+import logo from './logo.svg';
+import './App.css';
+
+const resList = [
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+  "id": "912754",
+  "name": "Urban Cafe",
+  "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/21/dbe91f1f-b400-4f4b-a78d-c6b99bdc61c5_912754.JPG",
+  "locality": "SOUTH CIVIL LINE",
+  "areaName": "Chhindwara City",
+  "costForTwo": "₹300 for two",
+  "cuisines": [
+  "Snacks",
+  "Pizzas",
+  "Pastas",
+  "Fast Food",
+  "Burgers",
+  "Cafe"
+  ],
+  "avgRating": 3.9,
+  "veg": true,
+  "parentId": "1330",
+  "avgRatingString": "3.9",
+  "totalRatingsString": "19",
+  "sla": {
+  "deliveryTime": 85,
+  "lastMileTravel": 13.9,
+  "serviceability": "SERVICEABLE",
+  "slaString": "83-93 mins",
+  "lastMileTravelString": "13.9 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2025-11-05 11:30:00",
+  "opened": true
+  },
+  "badges": {
+  "imageBadges": [
+  {
+  "imageId": "v1695133679/badges/Pure_Veg111.png",
+  "description": "pureveg"
+  }
+  ]
+  },
+  "isOpen": true,
+  "aggregatedDiscountInfoV2": {},
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {
+  "badgeObject": [
+  {
+  "attributes": {
+  "description": "pureveg",
+  "imageId": "v1695133679/badges/Pure_Veg111.png"
+  }
+  }
+  ]
+  },
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {},
+  "externalRatings": {
+  "aggregatedRating": {
+  "rating": "--"
+  }
+  },
+  "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+  "context": "seo-data-2caf90d5-ecfe-4102-bae8-3f19cce7be7e"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/city/chhindwara/urban-cafe-south-civil-line-chhindwara-city-rest912754",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "769851",
+"name": "Healthy Bites",
+"cloudinaryImageId": "4dfbe240cf1a2be01e6f2805c75c4279",
+"locality": "Collectrate Road",
+"areaName": "Chhindwara City",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Salads"
+],
+"veg": true,
+"parentId": "3143",
+"avgRatingString": "--",
+"sla": {
+"deliveryTime": 63,
+"lastMileTravel": 13.9,
+"serviceability": "SERVICEABLE",
+"slaString": "60-70 mins",
+"lastMileTravelString": "13.9 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 22:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "5.0",
+"ratingCount": "<3"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_EXTERNAL"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/healthy-bites-collectrate-road-chhindwara-city-rest769851",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "151656",
+"name": "Dev International",
+"cloudinaryImageId": "enj3srsnhbltbom2ovvh",
+"locality": "khajri road",
+"areaName": "Mohan Nagar",
+"costForTwo": "₹100 for two",
+"cuisines": [
+"Chinese",
+"Fast Food",
+"Beverages"
+],
+"avgRating": 4.2,
+"parentId": "71556",
+"avgRatingString": "4.2",
+"totalRatingsString": "605",
+"sla": {
+"deliveryTime": 67,
+"lastMileTravel": 13.6,
+"serviceability": "SERVICEABLE",
+"slaString": "65-75 mins",
+"lastMileTravelString": "13.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 22:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹100 OFF",
+"subHeader": "ABOVE ₹299",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/dev-international-khajri-road-mohan-nagar-rest151656",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "150602",
+"name": "Raimens Cafe 79",
+"cloudinaryImageId": "eohdm5sdnq6e2u46xeux",
+"locality": "Teacher's Colony",
+"areaName": "VIP Road",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Bakery",
+"Pizzas",
+"Snacks",
+"Beverages"
+],
+"avgRating": 4.4,
+"veg": true,
+"parentId": "164782",
+"avgRatingString": "4.4",
+"totalRatingsString": "4.8K+",
+"sla": {
+"deliveryTime": 67,
+"lastMileTravel": 14.4,
+"serviceability": "SERVICEABLE",
+"slaString": "65-75 mins",
+"lastMileTravelString": "14.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 22:30:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Rxawards/_CATEGORY-Desserts.png",
+"description": "Delivery!"
+},
+{
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/_Legendary%20Restaurants.png",
+"description": "Delivery!"
+},
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Rxawards/_CATEGORY-Desserts.png"
+}
+},
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/_Legendary%20Restaurants.png"
+}
+},
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/raimens-cafe-79-teachers-colony-vip-road-rest150602",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "774546",
+"name": "Shree Naivedyam",
+"cloudinaryImageId": "2274f59cb8efcfab1f446b195bcbda3b",
+"locality": "Khajri Road",
+"areaName": "Chhindwara City",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Chinese",
+"South Indian",
+"Pizzas",
+"Beverages"
+],
+"avgRating": 4.1,
+"veg": true,
+"parentId": "428968",
+"avgRatingString": "4.1",
+"totalRatingsString": "142",
+"sla": {
+"deliveryTime": 66,
+"lastMileTravel": 13.6,
+"serviceability": "SERVICEABLE",
+"slaString": "65-75 mins",
+"lastMileTravelString": "13.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹40 OFF",
+"subHeader": "ABOVE ₹399",
+"discountTag": "FLAT DEAL"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/shree-naivedyam-khajri-road-chhindwara-city-rest774546",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "151518",
+"name": "Bakery World",
+"cloudinaryImageId": "mt2aggiscfl3yviatwng",
+"locality": "Parasia Road",
+"areaName": "Parasia Road",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Bakery",
+"Ice Cream",
+"Snacks",
+"Beverages"
+],
+"avgRating": 4.3,
+"veg": true,
+"parentId": "40363",
+"avgRatingString": "4.3",
+"totalRatingsString": "296",
+"sla": {
+"deliveryTime": 62,
+"lastMileTravel": 14.1,
+"serviceability": "SERVICEABLE",
+"slaString": "60-70 mins",
+"lastMileTravelString": "14.1 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 22:30:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/bakery-world-parasia-road-rest151518",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "234875",
+"name": "Adil Hotel",
+"cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+"locality": "Rautha Wada",
+"areaName": "Chhindwara Locality",
+"costForTwo": "₹150 for two",
+"cuisines": [
+"Biryani",
+"Tandoor"
+],
+"avgRating": 4.4,
+"parentId": "27123",
+"avgRatingString": "4.4",
+"totalRatingsString": "1.4K+",
+"sla": {
+"deliveryTime": 61,
+"lastMileTravel": 12.6,
+"serviceability": "SERVICEABLE",
+"slaString": "60-70 mins",
+"lastMileTravelString": "12.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2025-11-05 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"aggregatedDiscountInfoV2": {},
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-d80ad0ab-3ad7-481b-9538-3761f66c85e3"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/chhindwara/adil-hotel-rautha-wada-chhindwara-locality-rest234875",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+];
+
+const Header = () => {
+  return(
+    <div className='header'>
+        <div className='logo-container'>
+          <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTlUPhdYRLORg_Su_UWfJ26H2T8VFPVx132g&s"/>
+        </div>
+        <div className='nav-items'>
+           <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li className="cart-container"><img className="cart-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAOltESk2kisSh56FwjCYJz1BAd8MIgxM9Cw&s"/></li>
+           </ul>
+        </div>
+    </div>
+  )
+} 
+const RestroCard = (props) => {
+  const {resData} = props;
+  const {cloudinaryImageId, name, avgRating, cuisines, costForTwo} = resData?.info;
+  return(
+    <div className='restro-card'>
+      <div className='restro-img-container'><img className="restro-img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +cloudinaryImageId}/></div>
+      <div className='card-container'>
+        <div className="restro-name">
+          <h3>{name}</h3>
+          <div className='restro-rating'><span class="badge">{avgRating}</span></div>
+        </div>
+        <p>{cuisines.join(",")}</p>
+        <div className='restro-cost'>
+          <h4>{costForTwo}</h4>
+          <div className='restro-del-time'><span>{resData?.info?.sla?.deliveryTime} Minutes</span></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Footer = () =>{
+  return(
+    <footer class="footer">
+  	 <div class="footer-container">
+  	 	<div class="footer-row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+  )
+}
+const Body = () => {
+  return(
+    <div className='restro-body-container'>
+       <div className='search'>Search</div>
+       <div className='restro-container'>
+        {
+          resList.map(restaurant => <RestroCard key={restaurant.info.id} resData={restaurant}/>)
+        }
+       </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className='app'>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default AppLayout;
